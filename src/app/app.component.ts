1f0c1863c2ee92b197eb8200f26afa7e4c904831
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-05 20:32:34 -0600

    Adding product component interaction with main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Product} from './product.model';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,44 @@ export class AppComponent {
 
   /* Angular components course logic */
   imgParent = '';
+
+  products: Product[] = [{
+    id: '1',
+    name: 'EL mejor juguete',
+    price: 565,
+    image: './assets/images/toy.jpg'
+  },
+  {
+    id: '2',
+    name: 'Bicicleta casi nueva',
+    price: 356,
+    image: './assets/images/bike.jpg'
+  },
+  {
+    id: '3',
+    name: 'Colleción de albumnes',
+    price: 34,
+    image: './assets/images/album.jpg'
+  },
+  {
+    id: '4',
+    name: 'Mis libros',
+    price: 23,
+    image: './assets/images/books.jpg'
+  },
+  {
+    id: '5',
+    name: 'Casa para perro',
+    price: 34,
+    image: './assets/images/house.jpg'
+  },
+  {
+    id: '6',
+    name: 'Gafas',
+    price: 3434,
+    image: './assets/images/glasses.jpg'
+  }
+];
 
   onLoaded(img: string){
     console.log('log padre ', img);
@@ -51,34 +89,40 @@ export class AppComponent {
     background: 'red'
   };
 
-  products: Product[] = [
+
+  productsOld: Product[] = [
     {
+      id: '1',
       name: 'EL mejor juguete',
       price: 565,
-      image: './assets/images/toy.jpg',
-      category: 'all'
+      image: './assets/images/toy.jpg'
     },
     {
+      id: '2',
       name: 'Bicicleta casi nueva',
       price: 356,
       image: './assets/images/bike.jpg'
     },
     {
+      id: '3',
       name: 'Colleción de albumnes',
       price: 34,
       image: './assets/images/album.jpg'
     },
     {
+      id: '4',
       name: 'Mis libros',
       price: 23,
       image: './assets/images/books.jpg'
     },
     {
+      id: '5',
       name: 'Casa para perro',
       price: 34,
       image: './assets/images/house.jpg'
     },
     {
+      id: '6',
       name: 'Gafas',
       price: 3434,
       image: './assets/images/glasses.jpg'
