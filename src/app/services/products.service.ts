88baeb7623ed9a69b28a789a58016cf6aa86111a
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-12 16:08:03 -0600

    Adding retry logic

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product.model';
 
 @Injectable({
@@ -20,7 +21,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset)
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   /*
