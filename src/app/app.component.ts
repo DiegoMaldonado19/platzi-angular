f05f498f2cc73fc97cd68161538e1255fbd64656
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-13 21:19:24 -0600

    File upload to a REST API using Angular HTTP, lamentably the Platzi API that I was using, is unavailable, so I can't test the request, but it should be working with any other api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,8 @@ export class AppComponent {
 
   token = '';
 
+  imgRta = '';
+
 
 
   onLoaded(img: string){
@@ -160,4 +162,15 @@ export class AppComponent {
     this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
     .subscribe();
   }
+
+  onUpload(event: Event){
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if(file){
+        this.filesService.uploadFile(file)
+      .subscribe( response => {
+        this.imgRta = response.location;
+      })
+    }
+    }
 }
--- src/app/services/files.service.ts
+++ src/app/services/files.service.ts
@@ -3,10 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { saveAs } from 'file-saver';
 import { tap, map } from 'rxjs/operators';
 
+interface File {
+  originalname: string;
+  filename: string;
+  location: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FilesService {
+
+  private apiUrl = 'https://api.escuelajs.co/files'
 
   constructor(
     private http: HttpClient
@@ -21,5 +29,15 @@ export class FilesService {
       }),
       map(() => true)
     );
+  }
+
+  uploadFile(file: Blob) {
+    const dto = new FormData();
+    dto.append('file', file);
+    return this.http.post<File>(`${this.apiUrl}/upload`, dto, {
+      //headers: {
+       // 'Content-type': "multipart/form-data"
+      //}
+    })
   }
 }
